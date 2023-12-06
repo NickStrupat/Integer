5c2d2ee9d8e8b6d6685518ea5ed1aa624463e135
NickStrupat <nick.strupat@gmail.com>
2023-12-05 21:05:18 -0500

    seal'em up

--- Static-Number-Class-Testing/Integer.cs
+++ Static-Number-Class-Testing/Integer.cs
@@ -1,81 +1,81 @@
-public class Integer<D> : IInteger
+public sealed class Integer<D> : IInteger
 where D : IIntegerDigit
 {
 	private Integer() {}
 	public static Int64 Value { get; } = D.Value;
 }
 
-public class Integer<D1, D2> : IInteger
+public sealed class Integer<D1, D2> : IInteger
 where D1 : IIntegerDigit where D2 : INaturalDigit
 {
 	private Integer() {}
 	public static Int64 Value { get; } = IInteger.Combine<D1, D2>();
 }
 
-public class Integer<D1, D2, D3> : IInteger
+public sealed class Integer<D1, D2, D3> : IInteger
 where D1 : IIntegerDigit where D2 : INaturalDigit where D3 : INaturalDigit
 {
 	private Integer() {}
 	public static Int64 Value { get; } = IInteger.Combine<Integer<D1, D2>, D3>();
 }
 
-public class Integer<D1, D2, D3, D4> : IInteger
+public sealed class Integer<D1, D2, D3, D4> : IInteger
 where D1 : IIntegerDigit where D2 : INaturalDigit where D3 : INaturalDigit where D4 : INaturalDigit
 {
 	private Integer() {}
 	public static Int64 Value { get; } = IInteger.Combine<Integer<D1, D2, D3>, D4>();
 }
 
-public class Integer<D1, D2, D3, D4, D5> : IInteger
+public sealed class Integer<D1, D2, D3, D4, D5> : IInteger
 where D1 : IIntegerDigit where D2 : INaturalDigit where D3 : INaturalDigit where D4 : INaturalDigit where D5 : INaturalDigit
 {
 	private Integer() {}
 	public static Int64 Value { get; } = IInteger.Combine<Integer<D1, D2, D3, D4>, D5>();
 }
 
-public class Integer<D1, D2, D3, D4, D5, D6> : IInteger
+public sealed class Integer<D1, D2, D3, D4, D5, D6> : IInteger
 where D1 : IIntegerDigit where D2 : INaturalDigit where D3 : INaturalDigit where D4 : INaturalDigit where D5 : INaturalDigit where D6 : INaturalDigit
 {
 	private Integer() {}
 	public static Int64 Value { get; } = IInteger.Combine<Integer<D1, D2, D3, D4, D5>, D6>();
 }
 
-public class Integer<D1, D2, D3, D4, D5, D6, D7> : IInteger
+public sealed class Integer<D1, D2, D3, D4, D5, D6, D7> : IInteger
 where D1 : IIntegerDigit where D2 : INaturalDigit where D3 : INaturalDigit where D4 : INaturalDigit where D5 : INaturalDigit where D6 : INaturalDigit where D7 : INaturalDigit
 {
 	private Integer() {}
 	public static Int64 Value { get; } = IInteger.Combine<Integer<D1, D2, D3, D4, D5, D6>, D7>();
 }
 
-public class Integer<D1, D2, D3, D4, D5, D6, D7, D8> : IInteger
+public sealed class Integer<D1, D2, D3, D4, D5, D6, D7, D8> : IInteger
 where D1 : IIntegerDigit where D2 : INaturalDigit where D3 : INaturalDigit where D4 : INaturalDigit where D5 : INaturalDigit where D6 : INaturalDigit where D7 : INaturalDigit where D8 : INaturalDigit
 {
 	private Integer() {}
 	public static Int64 Value { get; } = IInteger.Combine<Integer<D1, D2, D3, D4, D5, D6, D7>, D8>();
 }
 
-public class Integer<D1, D2, D3, D4, D5, D6, D7, D8, D9> : IInteger
+public sealed class Integer<D1, D2, D3, D4, D5, D6, D7, D8, D9> : IInteger
 where D1 : IIntegerDigit where D2 : INaturalDigit where D3 : INaturalDigit where D4 : INaturalDigit where D5 : INaturalDigit where D6 : INaturalDigit where D7 : INaturalDigit where D8 : INaturalDigit where D9 : INaturalDigit
 {
 	private Integer() {}
 	public static Int64 Value { get; } = IInteger.Combine<Integer<D1, D2, D3, D4, D5, D6, D7, D8>, D9>();
 }
 
-public class Integer<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10> : IInteger
+public sealed class Integer<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10> : IInteger
 where D1 : IIntegerDigit where D2 : INaturalDigit where D3 : INaturalDigit where D4 : INaturalDigit where D5 : INaturalDigit where D6 : INaturalDigit where D7 : INaturalDigit where D8 : INaturalDigit where D9 : INaturalDigit where D10 : INaturalDigit
 {
 	private Integer() {}
 	public static Int64 Value { get; } = IInteger.Combine<Integer<D1, D2, D3, D4, D5, D6, D7, D8, D9>, D10>();
 }
 
-public class Integer<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11> : IInteger
+public sealed class Integer<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11> : IInteger
 where D1 : IIntegerDigit where D2 : INaturalDigit where D3 : INaturalDigit where D4 : INaturalDigit where D5 : INaturalDigit where D6 : INaturalDigit where D7 : INaturalDigit where D8 : INaturalDigit where D9 : INaturalDigit where D10 : INaturalDigit where D11 : INaturalDigit
 {
 	private Integer() {}
 	public static Int64 Value { get; } = IInteger.Combine<Integer<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10>, D11>();
 }
 
-public class Integer<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12> : IInteger
+public sealed class Integer<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12> : IInteger
 where D1 : IIntegerDigit where D2 : INaturalDigit where D3 : INaturalDigit where D4 : INaturalDigit where D5 : INaturalDigit where D6 : INaturalDigit where D7 : INaturalDigit where D8 : INaturalDigit where D9 : INaturalDigit where D10 : INaturalDigit where D11 : INaturalDigit where D12 : INaturalDigit
 {
 	private Integer() {}
